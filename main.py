9b9e41ddd23001f227fbfea7882de7b4f6def3c4
William Blais <contact@william.tf>
2018-07-24 06:24:16 -0400

    - Use Json instead of .ini - Can monitor multiple rigs - Sentry

--- main.py
+++ main.py
@@ -1,85 +1,87 @@
-import configparser
 import json
 import sys
 import time
 
 import requests
+from raven import Client
 
 from miners.bminer import BMiner
 from miners.claymore import ClaymoreMiner
 
+# Sentry
+client = Client('https://0813bc02480642a99a2338ef50250072:41bf8af4deea4c3b9a7799582e194546@sentry.io/1248845')
+
 version = '0.1'
 api_url = 'https://api.rigmonitor.io/api/external'
 
-# Load Config
-config = configparser.ConfigParser()
 
 try:
-    config.read_file(open('config.ini'))
+    config_raw = open('config.json')
+    config = json.load(config_raw)
 except:
     print(
-        'Cloud not load config file. Please download your config.ini file from RigMonitor.io and copy it in this program directory.')
+        'Could not load config file. Please download your config.json file from RigMonitor.io and copy it in this program directory.')
     input("Press enter to exit")
     sys.exit(1)
 
-api_key = config.get('GENERAL', 'API_KEY')
-type = config.get('MINER', 'TYPE')
-host = config.get('MINER', 'HOST')
-port = int(config.get('MINER', 'PORT'))
-password = config.get('MINER', 'PASSWORD')
-
-if not api_key.strip():
+if not config['api_key']:
     print(
-        'Missing API key. Please download your config.ini file from RigMonitor.io and copy it in this program directory.')
+        'Missing API key. Please download your config.json file from RigMonitor.io and copy it in this program directory.')
     input("Press enter to exit")
     sys.exit(1)
+
+client.user_context({
+    'api_key': config['api_key'],
+    'rigs': config['rigs'],
+    'version': version
+})
+
 
 print(f'Welcome to RigMonitor.io version {version}')
 print('Please send any feedback or bug report to contact@ostrich.media')
-
 while 1:
-    print(f'Connecting to {type}...')
+    for rig in config["rigs"]:
+        print(f'Connecting to rig #{rig["id"]} {rig["type"]}...')
+        data = []
+        # Claymore
+        if rig['type'] == 'CLAYMORE':
+            data = ClaymoreMiner(rig['host'], rig['port'], rig['password'], client).getStats()
+        # BMiner
+        if rig['type'] == 'BMINER':
+            data = BMiner(rig['host'], rig['port'], rig['password'], client).getStats()
 
-    # Claymore
-    if type == 'CLAYMORE':
-        data = ClaymoreMiner(host, port, password).getStats()
-    # BMiner
-    if type == 'BMINER':
-        data = BMiner(host, port, password).getStats()
-
-    if data is None:
-        print('Miner Type not supported or no response from getStats().')
-        input("Press enter to exit")
-        sys.exit(1)
-
-    # add identification to data
-    identification = [{
-        'api_key': api_key,
-        'rig_id': config.get('GENERAL', 'RIG_ID'),
-        'miner': type,
-        'version': version
-    }]
-    data[0]['identification'] = identification
-
-    # Debug
-    if config.getboolean('GENERAL', 'DEBUG'):
-        print(json.dumps(data))
-    else:
-        print(f'Data successfully fetched from {type}')
-
-    # Send data to API
-    try:
-        r = requests.post(f'{api_url}/ingest', json=data)
-    except Exception as e:
-        print('Failed to send data to RigMonitor... Retrying.' + str(e))
-
-    if r.status_code != 200:
-        if r.status_code == 400:
-            print(r.json()['message'])
+        if not data:
+            print('Miner Type not supported or no response from getStats().')
         else:
-            print(f'Failed to send data to RigMonitor (status_code {r.status_code})... Retrying.')
-    else:
-        print(r.json()['message'])
+            # add identification to data
+            identification = [{
+                'api_key': config['api_key'],
+                'rig_id': rig['id'],
+                'miner': type,
+                'version': version
+            }]
+            data[0]['identification'] = identification
 
-    print('Waiting 1 minute')
-    time.sleep(60)
+            # Debug
+            if config['debug']:
+                print(json.dumps(data))
+            else:
+                print(f'Data successfully fetched from {type}')
+
+            # Send data to API
+            try:
+                r = requests.post(f'{api_url}/ingest', json=data)
+            except Exception as e:
+                client.captureException()
+                print('Failed to send data to RigMonitor... Retrying.' + str(e))
+
+            if r.status_code != 200:
+                if r.status_code == 400:
+                    print(r.json()['message'])
+                else:
+                    print(f'Failed to send data to RigMonitor (status_code {r.status_code})... Retrying.')
+            else:
+                print(r.json()['message'])
+
+    print('Waiting')
+    time.sleep(60 / len(config['rigs']))
--- miners/bminer.py
+++ miners/bminer.py
@@ -1,13 +1,13 @@
 import json
-import sys
 from urllib.request import urlopen
 
 
 class BMiner(object):
-    def __init__(self, host, port, password):
+    def __init__(self, host, port, password, client):
         self.host = host
         self.port = port
         self.password = password
+        self.client = client
 
     def getData(self):
         rig_json_data = []
@@ -18,44 +18,43 @@ class BMiner(object):
             rig_json_data = json.loads(rig_data.read().decode("utf-8"))
         except Exception as x:
             print("Error: Could not connect to rig. Please check the configuration file.")
-            input("Press enter to exit")
-            sys.exit(1)
 
         return rig_json_data
 
     def getStats(self):
         data = []
+        rig_json_data = []
         rig_json_data = self.getData()
-        try:
-            # Insert gpu measurement
-            rig_hashrate = 0;
-            gpu_data = []
-            for miner in rig_json_data["miners"]:
-                gpu = [
+        if rig_json_data:
+            try:
+                # Insert gpu measurement
+                rig_hashrate = 0;
+                gpu_data = []
+                for miner in rig_json_data["miners"]:
+                    gpu = [
+                        {
+                            "temperature": float(rig_json_data["miners"][miner]['device']["temperature"]),
+                            "fan": float(rig_json_data["miners"][miner]['device']['fan_speed']),
+                            "power": int(rig_json_data["miners"][miner]['device']['power']),
+                            "main_hashrate": float(rig_json_data["miners"][miner]['solver']['solution_rate'])
+                        }
+                    ]
+                    gpu_data.append(gpu)
+                    rig_hashrate = rig_hashrate + rig_json_data["miners"][miner]['solver']['solution_rate']
+
+                # Insert miner measurement
+                miner_data = [
                     {
-                        "temperature": float(rig_json_data["miners"][miner]['device']["temperature"]),
-                        "fan": float(rig_json_data["miners"][miner]['device']['fan_speed']),
-                        "power": int(rig_json_data["miners"][miner]['device']['power']),
-                        "main_hashrate": float(rig_json_data["miners"][miner]['solver']['solution_rate'])
+                        "main_hashrate": float(rig_hashrate),
+                        "main_accepted_shares": int(rig_json_data['stratum']['accepted_shares']),
+                        "main_rejected_shares": int(rig_json_data['stratum']['rejected_shares']),
+                        "current_mining_pool": str(rig_json_data['algorithm']),
                     }
                 ]
-                gpu_data.append(gpu)
-                rig_hashrate = rig_hashrate + rig_json_data["miners"][miner]['solver']['solution_rate']
 
-            # Insert miner measurement
-            miner_data = [
-                {
-                    "main_hashrate": float(rig_hashrate),
-                    "main_accepted_shares": int(rig_json_data['stratum']['accepted_shares']),
-                    "main_rejected_shares": int(rig_json_data['stratum']['rejected_shares']),
-                    "current_mining_pool": str(rig_json_data['algorithm']),
-                }
-            ]
-
-            data = [{"gpu": gpu_data, "miner": miner_data}]
-        except Exception as x:
-            print("Failed to decode miner data " + str(x))
-            input("Press enter to exit")
-            sys.exit(1)
+                data = [{"gpu": gpu_data, "miner": miner_data}]
+            except Exception as x:
+                print("Failed to decode miner data " + str(x))
+                self.client.captureException()
 
         return data
